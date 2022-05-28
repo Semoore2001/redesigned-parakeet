f8c703e7a66d1b43aaa37e136b971f9fe2c88e84
Semoore2001 <58708011+Semoore2001@users.noreply.github.com>
2022-05-28 18:26:10 +0100

    Find Anagrams

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
-# Read text from a file, and count the occurence of words in that text
+# Check if two words are anagrams 
 # Example:
-# count_words("The cake is done. It is a big cake!") 
-# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
 
-def read_file_content(filename):
-    file = open(filename,"r")
-    data = file.read()
-    return data
+def find_anagram(word,anagram):    
+    check = len(word) ==len(anagram)
 
-def count_words(filename):
-    text = read_file_content(filename)
-    words = text.split()
-    count= {}
-    for i in words:
-        if i in count:
-            count[i] += 1
+    if check == True:
+        count1 = {}
+        for i in word :
+            if i in count1:
+                count1[i] += 1
+            else:
+                count1[i] = 1
+        #print(count1)
+
+        count2 = {}
+        for i in anagram :
+            if i in count2:
+                count2[i] += 1
+            else:
+                count2[i] = 1
+        #print(count2)
+
+        if count1 == count2:
+            print("True")
         else:
-            count[i] = 1
-    return count
+            print("False")
+    
+    else:
+        print("False")
 
-print(count_words("Reading-Text-Files\story.txt"))
+
+find_anagram("hello", "check")
+find_anagram("below", "elbow") 
+    
+        
+
+
+
+
+
+
+
+           
+
+                    
+
+                   
+                
+        
+
+    
+    
+
